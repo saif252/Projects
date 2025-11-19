cbc1931733ceed0e9ce495bf65beab1924466454
Shawkatul Islam Sarker Saif <56231332+saif252@users.noreply.github.com>
2025-11-19 14:09:40 +0800

    Implement account creation and transaction logging
    Added account details structure and transaction logging.

--- Bank/Bank.c
+++ Bank/Bank.c
@@ -5,10 +5,45 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct acc_details
+{
+    char name[50];
+    int ID;
+    int acc_number;
+    int type;
+    float balance;
+} details;
+
+
+
 void folder()
 {
     mkdir("database", 0755);
+
+    char indexPath[] = "database/index.txt";
+    char transactionPath[] = "database/Transaction.log";
+
+    FILE *indexFile = fopen(indexPath, "r");
+    if (indexFile == NULL) 
+    {
+        indexFile = fopen(indexPath, "w");
+        if (indexFile == NULL) {
+            printf("Error creating %s\n", indexPath);
+        }
+    }
+    if (indexFile != NULL) fclose(indexFile);
+
+    FILE *transactionFile = fopen(transactionPath, "r");
+    if (transactionFile == NULL) 
+    {
+        transactionFile = fopen(transactionPath, "w");
+        if (transactionFile == NULL) {
+            printf("Error creating %s\n", transactionPath);
+        }
+    }
+    if (transactionFile != NULL) fclose(transactionFile);
 }
+
 
 void session_info()
 {
@@ -44,6 +79,38 @@ void session_info()
 
 void create()
 {
+    details account;
+
+    printf("\n");
+    printf("        Creating Account        \n");
+    printf("-----------------------------------\n");
+    printf("Name: ");
+    while (getchar() != '\n');
+    scanf("%s", account.name);
+
+    while (getchar() != '\n');
+    printf("ID: ");
+    scanf("%d", &account.ID);
+
+    printf("Account Type: ");
+    scanf("%d", &account.type);
+
+    account.balance = 0.0;
+
+    srand(time(NULL));
+    account.acc_number = 1000000 + rand() % 9000000;
+
+
+    printf("-----------------------------------\n");
+
+    printf("            Account Information        \n");
+    printf("-----------------------------------\n");
+    printf("Name: %s\n", account.name);
+    printf("ID: %d\n", account.ID);
+    printf("Account Type: %d\n", account.type);
+    printf("Account Number: %d\n", account.acc_number);
+    printf("Balance: %.2f\n", account.balance);
+    printf("-----------------------------------\n");
 
 }
 
@@ -73,6 +140,7 @@ void menu()
     
     while(1)
     {
+        printf("\n");
         printf("        Welcome To Bank     \n");
         printf("-----------------------------------\n");
         printf("Please Choose An Option\n");
@@ -91,26 +159,44 @@ void menu()
         if( strcmp(input, "1") == 0 || strcmp(input, "create") == 0 )
         {
             create();
+            FILE *transaction = fopen("database/Transaction.log", "a");
+            fprintf(transaction, "Create");
+            fclose(transaction);
         }
         else if ( strcmp(input, "2") == 0 || strcmp(input, "delete") == 0 )
         {
             delete();
+            FILE *transaction = fopen("database/Transaction.log", "a");
+            fprintf(transaction, "Delete");
+            fclose(transaction);
         }
         else if ( strcmp(input, "3") == 0 || strcmp(input, "deposit") == 0 )
         {
             deposit();
+            FILE *transaction = fopen("database/Transaction.log", "a");
+            fprintf(transaction, "Deposit");
+            fclose(transaction);
         } 
         else if ( strcmp(input, "4") == 0 || strcmp(input, "withdraw") == 0 )
         {
             withdraw();
+            FILE *transaction = fopen("database/Transaction.log", "a");
+            fprintf(transaction, "Withdraw");
+            fclose(transaction);
         } 
         else if ( strcmp(input, "5") == 0 || strcmp(input, "remittance") == 0 )
         {
             remittance();
+            FILE *transaction = fopen("database/Transaction.log", "a");
+            fprintf(transaction, "Remittance");
+            fclose(transaction);
         }
         else if ( strcmp(input, "6") == 0 || strcmp(input, "quit") == 0 )
         {
             printf("Qitted Successfully\n");
+            FILE *transaction = fopen("database/Transaction.log", "a");
+            fprintf(transaction, "Quit");
+            fclose(transaction);
             exit(0);
         }
         else 
